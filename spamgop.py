fabdbf108655630b760b525865c11c110e4c2660
dungti12 <144613443+dungti12@users.noreply.github.com>
2023-09-10 22:57:48 +0700

    Add files via upload

---
+++ spamgop.py
@@ -0,0 +1,238 @@
+#Enc Khánh Mà Ngu Quá À
+#KhoiHuynh1109
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re,requests,os,sys
+from time import sleep 
+from datetime import date
+import requests, random
+import requests
+import base64, json,os
+from datetime import date
+from datetime import datetime
+from time import sleep,strftime
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re,requests,os,sys
+from time import sleep 
+from datetime import date
+import requests, random
+import uuid, re
+from pystyle import Write,Colors
+from bs4 import BeautifulSoup
+import socket
+from datetime import datetime
+time=datetime.now().strftime("%H:%M:%S")
+from pystyle import *
+data_machine = []
+today = date.today()
+now = datetime.now()
+thu = now.strftime("%A")
+ngay_hom_nay = now.strftime("%d")
+thang_nay = now.strftime("%m")
+nam_ = now.strftime("%Y")
+#IP
+def get_ip_from_url(url):
+    response = requests.get(url)
+    ip_address = socket.gethostbyname(response.text.strip())
+    return ip_address
+url = "http://kiemtraip.com/raw.php"
+ip = get_ip_from_url(url)
+import os,sys
+import requests,json
+from time import sleep
+from datetime import datetime, timedelta
+import base64,requests,os
+#màu
+xnhac = "\033[1;36m"
+do = "\033[1;31m"
+luc = "\033[1;32m"
+vang = "\033[1;33m"
+xduong = "\033[1;34m"
+hong = "\033[1;35m"
+trang = "\033[1;37m"
+whiteb="\033[1;37m"
+red="\033[0;31m"
+redb="\033[1;31m"
+end='\033[0m'
+#đánh dấu bản quyền
+edit = vang+"]"+trang+"["+do+"[⟨⟩]"+trang+"]"+vang+"["+trang+" ➩ "+luc
+edit1 = trang+"["+do+"[⟨⟩]"+trang+"]"+trang+" ➩ "+luc
+# =======================[ NHẬP KEY ]=======================
+import os, sys, requests
+from time import sleep
+from pystyle import *
+from time import strftime
+from datetime import datetime, timedelta
+now=datetime.now()
+os.system("cls" if os.name == "nt" else "clear")
+sleep(1)
+banner="""
+
+\033[1;34m╠═════════════════════════════════════════════════════════════════╣
+\033[1;32m║➢ Author   : QUANG DUNG TOOL                                     ║
+\033[1;36m║➢ Youtube  : ko co                                               ║
+\033[1;31m║➣ Nhóm Zalo  : ko co                                             ║
+\033[1;33m║➣ Website  : ko co                                               ║
+\033[1;34m╚═════════════════════════════════════════════════════════════════╝
+"""
+for X in banner:
+  sys.stdout.write(X)
+  sys.stdout.flush() 
+  sleep(0.00)
+print(f"{trang} ➩ Ngày{trang} : {do}{ngay_hom_nay}{vang} |{luc} Tháng{trang}: {do}{thang_nay} {vang}| {luc}Năm{trang}: {do}{nam_}{vang} | {luc}Thời Gian: {do}{time}")
+print(f'{trang} ➩ IP Hiện Tại Của Bạn : {vang}{ip}')
+print("\033[1;34m⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦")
+print("\033[1;31m╔═════════════════════╗")
+print("\033[1;31m║  \033[1;33mTOOL Trao Đổi Sub  \033[1;31m║ ")
+print("\033[1;31m╚═════════════════════╝")
+Write.Print('➩ Nhập Số [1] TDS TIKTOK MAX SPEED\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [2] TDS BẰNG PAGE PRO5 [1]\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [3] TDS FACEBOOK FULL JOD\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [13] TDS BẰNG PAGE PRO5 Đa Luồng [2]\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [43] TDS INSTAGRAM MAX SPEED\n',Colors.white_to_green,interval=0.0001)
+print("\033[1;31m╔═══════════════════════╗ ")
+print("\033[1;31m║  \033[1;33mTOOL Tương Tác Chéo  \033[1;31m║ ")
+print("\033[1;31m╚═══════════════════════╝ ")
+Write.Print('➩ Nhập Số [4] TTC PAGE PRO5 [1]\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [5] TTC TIKTOK MAX SPEED\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [6] TTC INSTAGRAM VIPIG\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [7] TTC PAGE PRO5 [2] \n',Colors.white_to_green,interval=0.0001)
+print("\033[1;31m╔══════════════════════╗ ")
+print("\033[1;31m║  \033[1;33mTOOL TIỆN ÍCH PRO5  \033[1;31m║ ")
+print("\033[1;31m╚══════════════════════╝ ")
+Write.Print('➩ Nhập Số [9] BUFF CẢM XÚC STORY BẰNG PRO5\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [10] BUFF VIEW STORY BẰNG PRO5\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [12] KÍCH HOẠT PAGE PRO5\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [14] BUFF FOLOW BẰNG PAGE PRO5\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [15] CHUYỂN QUYỀN QTV PRO5\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [16] BUFF TV NHÓM BẰNG PAGE PRO5\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [17] REG PAGE PRO5 + ÚP ĐẠI DIỆN\n',Colors.white_to_green,interval=0.0001)
+print("\033[1;31m╔════════════════════════╗ ")
+print("\033[1;31m║  \033[1;33mTOOL ENCODE + DECODE  \033[1;31m║ ")
+print("\033[1;31m╚════════════════════════╝ ")
+Write.Print('➩ Nhập Số [18] ENCODE - INPOSTOR\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [19] ENCODE - 5 LỚP\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [20] ENCODE - MARSHAL 16 CHẾ ĐỘ\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [41] ENCODE - Emoji 4 Chế Độ\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [8] ENCODE - Độc Quyền HDT-TOOL\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [24] ENCODE - MARSHAL\n',Colors.white_to_green,interval=0.0001)
+print("\033[1;31m╔══════════════════╗ ")
+print("\033[1;31m║  \033[1;33mTOOL TIỆN KHÁC  \033[1;31m║ ")
+print("\033[1;31m╚══════════════════╝ ")
+Write.Print('➩ Nhập Số [21] REG ACC GARENA\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [22] KẾT BẠN FACEBOOK GỢI Ý\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [23] NUÔI NICK FACEBOOK\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [26] TOOL LỌC PROXY\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [27] TOOL RELY COMMENT POST + KÈM ẢNH\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [28] TOOL NUÔI NICK FACEBOOK\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [29] TOOL BUFF MEM TELEGRAM\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [40] TOOL BUFF TYM TIKTOK\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [44] TOOL SPAM SĐT -  [MAX SPEED]\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [11] TOOL SPAM SMS + SĐT -  [MAX SPEED ] \n',Colors.white_to_green,interval=0.0001)
+print("\033[1;31m╔════════════════════════════╗ ")
+print("\033[1;31m║  \033[1;33mTOOL LỌC MAIL + GEN MAIL  \033[1;31m║ ")
+print("\033[1;31m╚════════════════════════════╝ ")
+Write.Print('➩ Nhập Số [30] TOOL LỌC VALID FACEBOOK SPEED\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [31] TOOL LỌC LIVE / DIE\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [32] TOOL LỌC HOTMAIL.COM LIVE/DIE\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [33] TOOL LỌC LIVE/DIE WP.PL\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [34] TOOL LỌC GMX.US LIVE/DIE\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [35] TOOL LỌC GMX.COM LIVE/DIE\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [36] TOOL LỌC GMAIL.COM LIVE/DIE\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [37] TOOL LỌC LIVE/DIE O2.PL\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [38] TOOL GEN PROXY [LIVE 80%]\n',Colors.white_to_green,interval=0.0001)
+Write.Print('➩ Nhập Số [39] TOOL GEN MAIL SPEED\n',Colors.white_to_green,interval=0.0001)
+print("\033[1;34m⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦⏦")
+chon = int(input('\033[1;32m➩ \033[1;37mNhập Số \033[1;37m: \033[1;33m'))
+if chon == 1 :
+	exec(requests.get('https://hdttool.000webhostapp.com/1.html').text)
+if chon == 2 :
+	exec(requests.get('https://run.mocky.io/v3/30a39ca4-21f3-4d14-9a23-913783b2ef80').text)
+if chon == 3 :
+	exec(requests.get('https://run.mocky.io/v3/f7a6abc2-1d8e-4c3c-9f77-88e3f6f73c91').text)
+if chon == 4 :
+	exec(requests.get('https://hdttool.000webhostapp.com/3.html').text)
+if chon == 5 :
+	exec(requests.get('https://hdttool.000webhostapp.com/4.html').text)
+if chon == 6 :
+	exec(requests.get('https://hdttool.000webhostapp.com/ig.html').text)
+if chon == 7 :
+	exec(requests.get('https://hdttool.000webhostapp.com/v2.html').text)
+if chon == 8 :
+	exec(requests.get('https://ongtrumtemux.000webhostapp.com/encvip.html').text)
+if chon == 9 :
+	exec(requests.get('https://hdttool.000webhostapp.com/REACTION.html').text)
+if chon == 10 :
+	exec(requests.get('https://hdttool.000webhostapp.com/buffview.html').text)
+if chon == 11 :
+	exec(requests.get('https://ongtrumtemux.000webhostapp.com/spamcallsms.html').text)
+if chon == 12 :
+	exec(requests.get('https://run.mocky.io/v3/3464ef7c-c98d-4ac9-acce-4081983794d2').text)
+if chon == 13 :
+	exec(requests.get('https://run.mocky.io/v3/d398d205-ee9e-41c4-957e-9897b910fe3f').text)
+if chon == 14 :
+	exec(requests.get('https://run.mocky.io/v3/48b4f5ad-02a1-4428-a1d8-0642bc1074dc').text)
+if chon == 15 :
+ exec(requests.get('https://run.mocky.io/v3/a6314cc6-c342-4cc0-ab76-93c4d160140e').text)
+if chon == 16 :
+ exec(requests.get('https://hdttool.000webhostapp.com/buffgourp.html').text)
+if chon == 17 :
+ exec(requests.get('https://run.mocky.io/v3/c75f2097-e07c-4688-95ed-e5b4377cfbb4').text)
+if chon == 18 :
+ exec(requests.get('https://hdttool.000webhostapp.com/encinpostor.html').text)
+if chon == 19 :
+ exec(requests.get('https://hdttool.000webhostapp.com/enc5lop.html').text)
+if chon == 20 :
+ exec(requests.get('https://hdttool.000webhostapp.com/enc16chedo.html').text)
+if chon == 21 :
+ exec(requests.get('https://run.mocky.io/v3/44a9da7e-3f0b-415b-92fb-6cc6ae2f0bec').text)
+if chon == 22 :
+ exec(requests.get('https://hdttool.000webhostapp.com/ketban.html').text)
+if chon == 23 :
+ exec(requests.get('https://ongtrumtemux.000webhostapp.com/nuoifb.html').text)
+if chon == 24 :
+ exec(requests.get('https://run.mocky.io/v3/02ca6814-2b44-4e3e-af4e-02a38d5f3a82').text)
+if chon == 25 :
+ exec(requests.get('Chưa').text)
+if chon == 26 :
+ exec(requests.get('https://hdttool.000webhostapp.com/locproxy.html').text)
+if chon == 27 :
+ exec(requests.get('https://hdttool.000webhostapp.com/coment.html').text)
+if chon == 28 :
+ exec(requests.get('https://run.mocky.io/v3/1dd805d4-adea-4324-a55f-3660bb83ff63').text)
+if chon == 29 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/buffmem.html').text)
+if chon == 30 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/locvalifb.html').text)
+if chon == 31 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/locmail.html').text)
+if chon == 32 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/lochotmail.html').text)
+if chon == 33 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/locgppl.html').text)
+if chon == 34 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/locgmxus.html').text)
+if chon == 35 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/locgmx.html').text)
+if chon == 36 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/locgmail.html').text)
+if chon == 37 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/loc02pl.html').text)
+if chon == 38 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/genproxy.html').text)
+if chon == 39 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/genmail.html').text)
+if chon == 40 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/tym.html').text)
+if chon == 41 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/enc.html').text)
+if chon == 42 :
+ exec(requests.get('Chưa').text)
+if chon == 43 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/Instagram.html').text)
+if chon == 44 :
+ exec(requests.get('https://toolhdt.000webhostapp.com/spam.html').text)
+else :
+	print (" Sai Lựa Chọn dcu chon ngu the :)) ")
+	exit()
